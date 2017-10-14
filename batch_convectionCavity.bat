cfa41c5b357a63fc81f2d97437bcc147b42fede5
dayalr <dayal.rd@gmail.com>
2017-10-14 14:18:21 +0530

    basic command files uploaded

---
+++ batch_convectionCavity.bat
@@ -0,0 +1,29 @@
+cd F:\convection_cavity\Ri_.1_res
+rename data_0.csv dump.csv
+dataReader.exe
+rename dump.csv data_0.csv
+rename tecplotdat.dat tecplot_0.dat
+rename data_30.csv dump.csv
+dataReader.exe
+rename dump.csv data_30.csv
+rename tecplotdat.dat tecplot_30.dat
+rename data_60.csv dump.csv
+dataReader.exe
+rename dump.csv data_60.csv
+rename tecplotdat.dat tecplot_60.dat
+rename data_90.csv dump.csv
+dataReader.exe
+rename dump.csv data_90.csv
+rename tecplotdat.dat tecplot_90.dat
+rename data_120.csv dump.csv
+dataReader.exe
+rename dump.csv data_120.csv
+rename tecplotdat.dat tecplot_120.dat
+rename data_150.csv dump.csv
+dataReader.exe
+rename dump.csv data_150.csv
+rename tecplotdat.dat tecplot_150.dat
+rename data_180.csv dump.csv
+dataReader.exe
+rename dump.csv data_180.csv
+rename tecplotdat.dat tecplot_180.dat
